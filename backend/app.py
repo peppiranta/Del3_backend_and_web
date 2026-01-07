bb9f81c30a8e98cf0b5c30ff2c3331e61334e67a
hamma1273 <151581036+hamma1273@users.noreply.github.com>
2026-01-07 20:41:07 +0000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,4 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route()
+@app.route("/test", methods =["GET"])
+def test():
+  return jsonify({"message" : "working"}), 200
+
+if __name__ == "__main__":
+  app.run(debug=TRUE):
